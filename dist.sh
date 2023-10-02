9b448d1f7498cb359ccee0c636314ed5794ff332
dan.markhasin <dan.markhasin@appsflyer.com>
2023-10-02 21:19:56 +0300

    fix dist script

--- dist.sh
+++ dist.sh
@@ -2,11 +2,12 @@
 
 # Check if the dist folder exists, if not create it
 if [ ! -d "dist" ]; then
-  mkdir dist
+  mkdir -p dist/src
+  mkdir -p dist/client/build
 fi
 
 # Copy the src folder and its content to the dist folder
-cp -r src dist/
+cp -r src dist/src
 
 # Copy the client folder and its content to the dist folder
 cp -r client/build dist/
