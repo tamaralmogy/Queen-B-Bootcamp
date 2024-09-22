1ee20cda93e27c25d83595aa9aace1a5a084e597
Tamar Almogy <tamar.almogy@post.runi.ac.il>
2024-09-22 15:00:43 +0300

    Commit changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import firstPerson from './images/person1.svg';
-import Header from './components/Header';
-import './App.css';
-import MentorCard from './components/MentorCard';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import firstPerson from "./images/person1.svg";
+import Header from "./components/Header";
+import "./App.css";
+import MentorCard from "./components/MentorCard";
 const port = process.env.PORT || 5001;
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    axios.get(`http://localhost:${port}/api/helloworld`)
-      .then(response => setMessage(response.data))
-      .catch(error => console.error(`There was an error retrieving the message: ${error}`))
-  }, [])
+    axios
+      .get(`http://localhost:${port}/api/helloworld`)
+      .then((response) => setMessage(response.data))
+      .catch((error) =>
+        console.error(`There was an error retrieving the message: ${error}`)
+      );
+  }, []);
 
   return (
     <div className="App">
-      <Header/>
-      
+      <Header />
+
       {/* add here upcoming events*/}
 
-      <input 
-          type="text" 
-          placeholder="Search by name or skill"
-          className="search-bar"
-        />
+      <input
+        type="text"
+        placeholder="Search by name or skill"
+        className="search-bar"
+      />
 
       <div className="mentor-list">
         {/* must pass on the database records and add as needed */}
