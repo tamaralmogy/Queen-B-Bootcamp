89ef9294de297d9bcab91a71ea4ca53b718bff4d
hayanat2002 <haya.natsheh@mail.huji.ac.il>
2024-09-23 22:37:44 +0300

    worked mentor cards

---
+++ src/database.sql
@@ -0,0 +1,21 @@
+CREATE TABLE "User" (
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(100),
+    lastName VARCHAR(100),
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role VARCHAR(50)
+);
+
+CREATE TABLE "Mentor" (
+    id SERIAL PRIMARY KEY,
+    userId INT REFERENCES "User" (id) ON DELETE CASCADE,  
+    avatar VARCHAR(255),
+    field VARCHAR(100),
+    phone VARCHAR(20),
+    linkedin VARCHAR(255),
+    github VARCHAR(255),
+    languages VARCHAR(255),
+    workplace VARCHAR(255),
+    linkToSchedule VARCHAR(255)
+);
